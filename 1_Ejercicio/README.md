bf4ee71ada922d28da3ada23002d565161fb44d2
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-06-29 19:38:06 -0500

    Se agrega instrucciones de creación del proyecto

--- 1_Ejercicio/README.md
+++ 1_Ejercicio/README.md
@@ -11,3 +11,9 @@ Example
     - webpack-dev-server => Para crear un servidor de desarrollo rapidamente en el archivo de configuracion.
     - babel-loader => Son cargadores. Dependiendo el tipo de archivo que estemos utilizando cargarlo y realizar las transformaciones necesarias.
     - babel-cli => es el core  de babel.
+
+---------------------------------------------------------------------------
+
+1. Creamos un archivo llamado webpack.config.js
+
+2. Agregamos las siguientes lineas al archivo package.json
---
+++ 1_Ejercicio/webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+	entry: {
+		app: './src/index.js'
+	},
+	output: {
+		path: './build',
+		filename: 'bundle.js',
+		publicPath: '/build/'
+    },
+	resolve: {
+		extensions: ['', '.js']
+	},
+    /* 
+        Nos crea un servidor que este creado
+        en NODE EXPRESS "inline: true "
+    */
+	devServer: {
+		host: '0.0.0.0',
+		port: 8080,
+		inline: true
+	},
+	module: {
+		loaders: [
+			{
+                /*
+                    expresion regular que nos
+                    tomen en cuenta todos los 
+                    archivos js
+                */ 
+				test: /(\.js|.jsx)$/,
+				loader: 'babel',
+				exclude: '/node_modules/',
+				query: {
+					presets: ['es2015']
+				}
+			}
+		]
+	}
+ }
+ 
